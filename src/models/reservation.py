bf0466fa7b7eda505b9d47a57c0cd42dff81e143
Santiago <smoyano@escuelasproa.edu.ar>
2024-10-14 10:04:41 -0300

    validaciones de reserva + expresion regular de 'link'

--- src/models/reservation.py
+++ src/models/reservation.py
@@ -1,5 +1,7 @@
 from db import db
 from sqlalchemy.sql import func
+import datetime
+from config import regular_exps
 
 class Reservation(db.Model):    
     id = db.Column(db.Integer, primary_key=True)
@@ -15,5 +17,32 @@ class Reservation(db.Model):
     created_at = db.Column(db.DateTime, default=func.now())
     updated_at = db.Column(db.DateTime, default=func.now())
     deleted_at = db.Column(db.DateTime)
+    @staticmethod
+    def from_reserva(eventhall_id, client_id, reservation_date, hora_reserva, url_payment=None, state="Pendiente", reservation_price=int, validated_by=None, validated_at=None, deleted_at=None):
+        # Validar si el precio es un número
+        if not isinstance(reservation_price, int):
+            return ["price must contain only numbers"]
+        if not isinstance(eventhall_id, int):
+            return ["eventhall_id must contain only numbers"]
+        if state != "Pendiente" and state != "Reservado":
+            return ["invalid state"]
 
-    # def validate_reserve( eventhall_id, client_id, reservation_date, hora_reserva):
+
+        # Asegurarse de que `reservation_date` y `hora_reserva` sean objetos datetime
+        if isinstance(reservation_date, str):
+            try:
+                reservation_date = datetime.strptime(reservation_date, '%Y-%m-%d %H:%M:%S')
+            except ValueError:
+                return ["Invalid date format for reservation_date, should be 'YYYY-MM-DD HH:MM:SS'"]
+
+        if isinstance(hora_reserva, str):
+            try:
+                hora_reserva = datetime.strptime(hora_reserva, '%Y-%m-%d %H:%M:%S')
+            except ValueError:
+                return ["Invalid date format for hora_reserva, should be 'YYYY-MM-DD HH:MM:SS'"]
+            
+        if regular_exps.link.match(url_payment) == None:
+            return ["Invalid url"]
+
+        # Crear el objeto Reservation
+        return [None, Reservation(eventhall_id=eventhall_id, client_id=client_id, reservation_date=reservation_date, hora_reserva=hora_reserva, url_payment=url_payment, state=state, reservation_price=reservation_price, validated_by=validated_by, validated_at=validated_at, deleted_at=deleted_at)]
