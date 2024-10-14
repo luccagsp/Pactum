70fe0a07734d0743191cb7da77f0b2bf9370d202
Santiago <smoyano@escuelasproa.edu.ar>
2024-10-14 09:46:50 -0300

    validacion de link

--- src/config/regular_exps.py
+++ src/config/regular_exps.py
@@ -1,3 +1,4 @@
 import re 
 email = re.compile(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$')
 name = re.compile(r'^[\w\s]+$')
+link = re.compile(r"((https?):((//)|(\\\\))+[\w\d:#@%/;$()~_?\+-=\\\.&]*)", re.MULTILINE|re.UNICODE)
